8b946dfefb23745524b05a563354ef56e822c630
L2ncE <llance_24@foxmail.com>
2022-02-15 12:03:34 +0800

    refactor(service):更改影人查询方式

--- service/celebrity.go
+++ service/celebrity.go
@@ -6,8 +6,8 @@ import (
 )
 
 // GetCelebrityById 通过id得到影人
-func GetCelebrityById(Id int) (model.Celebrity, error) {
-	return dao.SelectCelebrityById(Id)
+func GetCelebrityById(Id int, mid int) (model.Celebrity, error) {
+	return dao.SelectCelebrityById(Id, mid)
 }
 
 func GetSearch2(context string) ([]model.Search2, error) {
