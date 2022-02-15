cf0a22c8598f5780662c9548ea1a805ed9d3dde9
L2ncE <llance_24@foxmail.com>
2022-02-15 12:03:27 +0800

    refactor(dao):更改影人查询方式

--- dao/celebrity.go
+++ dao/celebrity.go
@@ -3,10 +3,10 @@ package dao
 import "douban/model"
 
 // SelectCelebrityById 通过id来搜索影人
-func SelectCelebrityById(Id int) (model.Celebrity, error) {
+func SelectCelebrityById(Id int, mid int) (model.Celebrity, error) {
 	var celebrity model.Celebrity
 
-	row := dB.QueryRow("SELECT id, name, info, synopsis, url, award, urlinfo1, urlinfo2, urlinfo3, nameinfo1, nameinfo2, nameinfo3, yearinfo FROM celebrity WHERE id = ? ", Id)
+	row := dB.QueryRow("SELECT id, name, info, synopsis, url, award, urlinfo1, urlinfo2, urlinfo3, nameinfo1, nameinfo2, nameinfo3, yearinfo FROM celebrity WHERE id = ? and movieId = ?", Id, mid)
 	if row.Err() != nil {
 		return celebrity, row.Err()
 	}
