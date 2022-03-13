bbb6983e3521b75e4c1385be8ddb227bdc755938
L2ncE <llance_24@foxmail.com>
2022-03-13 15:23:27 +0800

    feat(service):支持Github第三方登录

---
+++ service/oauth.go
@@ -0,0 +1,73 @@
+package service
+
+import (
+	"douban/model"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+var conf = model.Conf{
+	ClientId:     "a915f210ec53c297a8ea",
+	ClientSecret: "d39c4dd2ee8eb4d493b17745ac2db24f699be08f",
+	RedirectUrl:  "http://42.192.155.29:8080/oauth",
+}
+
+func GetTokenAuthUrl(code string) string {
+	return fmt.Sprintf(
+		"https://github.com/login/oauth/access_token?client_id=%s&client_secret=%s&code=%s",
+		conf.ClientId, conf.ClientSecret, code,
+	)
+}
+
+func GetToken(url string) (*model.Token, error) {
+
+	// 形成请求
+	var req *http.Request
+	var err error
+	if req, err = http.NewRequest(http.MethodGet, url, nil); err != nil {
+		return nil, err
+	}
+	req.Header.Set("accept", "application/json")
+
+	// 发送请求并获得响应
+	var httpClient = http.Client{}
+	var res *http.Response
+	if res, err = httpClient.Do(req); err != nil {
+		return nil, err
+	}
+
+	// 将响应体解析为 token，并返回
+	var token model.Token
+	if err = json.NewDecoder(res.Body).Decode(&token); err != nil {
+		return nil, err
+	}
+	return &token, nil
+}
+
+func GetUserInfo(token *model.Token) (map[string]interface{}, error) {
+
+	// 形成请求
+	var userInfoUrl = "https://api.github.com/user" // github用户信息获取接口
+	var req *http.Request
+	var err error
+	if req, err = http.NewRequest(http.MethodGet, userInfoUrl, nil); err != nil {
+		return nil, err
+	}
+	req.Header.Set("accept", "application/json")
+	req.Header.Set("Authorization", fmt.Sprintf("token %s", token.AccessToken))
+
+	// 发送请求并获取响应
+	var client = http.Client{}
+	var res *http.Response
+	if res, err = client.Do(req); err != nil {
+		return nil, err
+	}
+
+	// 将响应的数据写入 userInfo 中，并返回
+	var userInfo = make(map[string]interface{})
+	if err = json.NewDecoder(res.Body).Decode(&userInfo); err != nil {
+		return nil, err
+	}
+	return userInfo, nil
+}
