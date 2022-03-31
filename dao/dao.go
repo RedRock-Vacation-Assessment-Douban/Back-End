d5cd7a84336295736b144c0b977c40afd148e5ca
L2ncE <llance_24@foxmail.com>
2022-03-31 17:58:10 +0800

    feat(dao):更新GORM连接

--- dao/dao.go
+++ dao/dao.go
@@ -2,12 +2,16 @@ package dao
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/go-redis/redis"
 	_ "github.com/go-sql-driver/mysql"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 var dB *sql.DB
 var rdb *redis.Client
+var db *gorm.DB
 
 func InitDB() (err error) {
 	dsn := "lance:yxh030714@tcp(1.14.43.76:3306)/douban?charset=utf8&parseTime=True"
@@ -35,4 +39,21 @@ func InitRedis() (err error) {
 		return err
 	}
 	return nil
+}
+
+func InitGormDB() (err error) {
+	dB, err := gorm.Open(mysql.New(mysql.Config{
+		DSN:                      "lance:yxh030714@tcp(1.14.43.76:3306)/douban?charset=utf8mb4&parseTime=True&loc=Local", // DSN data source name
+		DefaultStringSize:        171,
+		DisableDatetimePrecision: true,
+		DontSupportRenameIndex:   true,
+	}), &gorm.Config{
+		SkipDefaultTransaction:                   false,
+		DisableForeignKeyConstraintWhenMigrating: true,
+	})
+	if err != nil {
+		fmt.Printf("连接失败：%v\n", err)
+	}
+	db = dB
+	return err
 }
