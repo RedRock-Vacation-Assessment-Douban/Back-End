de0243fa86ebb0f55d24f1beeda3c4ed30044bc1
L2ncE <llance_24@foxmail.com>
2022-03-31 17:57:39 +0800

    feat(cmd):使用GORM连接MySQL

--- cmd/main.go
+++ cmd/main.go
@@ -19,5 +19,11 @@ func main() {
 	} else {
 		fmt.Println("连接Redis数据库成功!")
 	}
+	err = dao.InitGormDB()
+	if err != nil {
+		fmt.Printf("init redis failed, err:%v\n", err)
+	} else {
+		fmt.Println("连接GORM MySQL数据库成功!")
+	}
 	api.InitEngine()
 }
