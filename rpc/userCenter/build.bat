7afe305e3726a3a2a429e9763b4436e36e04de3b
L2ncE <llance_24@foxmail.com>
2022-04-27 16:22:10 +0800

    feat(rpc):新增proto

---
+++ rpc/userCenter/build.bat
@@ -0,0 +1,2 @@
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative ./changePW/changePW.proto
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative ./userCenter/userCenter.proto
